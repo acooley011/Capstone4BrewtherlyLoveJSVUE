5f5399ade856bb1664b7f5bf3453d94ec65cab96
shantal gonzalez <shantal.gonzalez@gmail.com>
2020-04-21 19:35:49 -0400

    added page views for admin, brewer and beer lover

---
+++ frontend/src/views/Brewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="brewer"><h1>Brewer</h1>
+  <h2>Beer List</h2>
+  <button>View Brewery Information</button>
+  <button>Update Brewery Information</button>
+  <button>Add A Beer to Beer List</button>
+  <button>Remove A Beer from Beer List</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'brewer'
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/BreweryList.vue
+++ frontend/src/views/BreweryList.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="brewery-list">
     <h1>Brewery List</h1>
+    <ul>
+      <li>Yards</li>
+      <li>Evil Genius</li>
+      <li>LoveCity</li>
+      <li>DockStreet</li>
+      <li>ARS</li>
+      <li>Tired Hands</li>
+      <li>PBC</li>
+      <li>Original 13 Ciderworks</li>
+      <li>Seperatist</li>
+      <li>Crime and Punishment</li>
+
+    </ul>
   </div>
 </template>
 
@@ -9,3 +22,7 @@ export default {
   name: 'brewery-list'
 }
 </script>
+
+<style scoped>
+
+</style>
