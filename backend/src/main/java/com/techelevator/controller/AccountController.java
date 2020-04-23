9a547677072d14b07a7aea3f2c69226461121c06
Chet Sullivan <cksullivan13@gmail.com>
2020-04-23 12:54:05 -0400

    modified home page, app.vue and index

--- backend/src/main/java/com/techelevator/controller/AccountController.java
+++ backend/src/main/java/com/techelevator/controller/AccountController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  * AccountController
  */
 @RestController
+@CrossOrigin
 public class AccountController {
 
     @Autowired
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
 <header>
-    <ul class="nav">
-      <img alt="BrewMap logo" src="./assets/images/beermapplaceholder.png">
-      <li><router-link :to="{name: 'home'}" exact>Home</router-link></li>
-      <li><router-link :to="{name: 'brewery-list'}">Brewery List</router-link></li>
-      <li><router-link :to="{name: 'reviews'}">Reviews</router-link></li>
-      </ul>
-      <ul class="nav-right">
-      <li><router-link :to="{name: 'login'}" class="home">Login/Register</router-link></li>
-      </ul>
+    <nav class="breadcrumb is-centered has-dot-separator " aria-label="breadcrumbs">
+      <div id ="navbarBasicExample" class="navbar-menu ">
+        <div class="navbar-start is-size-5">
+      <li><router-link :to="{name: 'home'}">
+        <span class="icon is-small"><i class="fa fa-home" aria-hidden="true"></i></span><span>Home</span></router-link></li>
+      <li><router-link :to="{name: 'brewery-list'}">
+        <span class="icon is-small"><i class="fa fa-beer" aria-hidden="true"></i></span><span>Brewery List</span></router-link></li>
+      <li><router-link :to="{name: 'reviews'}">
+        <span class="icon is-small"><i class="fa fa-envelope-open" aria-hidden="true"></i></span><span>Reviews</span></router-link></li>
+     <li><router-link :to="{name: 'login'}">
+      <span class="icon is-small"><i class="fa fa-user-circle" aria-hidden="true"></i></span><span>Login/Register</span></router-link></li>
+        </div>
+        </div>
+      </nav>
     </header>
-    <div class="content">
+    <section>
+      <div>
+        </div>
+        </section>
     <router-view />
     </div>
-  </div>
+  
 </template>
 
 <style>
 
+/*
 body {
   margin: 0;
   padding: 0;
@@ -31,14 +40,14 @@ body {
   background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
   font-family: 'Roboto', sans-serif;
 }
-
+*/
 /*#app {
   width: 80vw;
   height: 100%;
   background-color: white;
 }*/
 
-header {
+/*header {
   height: 50px;
   width: 100%;
   display: flex;
@@ -54,8 +63,8 @@ header {
   margin:0;
   flex:1;
   list-style-type: none;
-}
-
+}*/
+/*
 .nav li {
   display: inline;
   padding-right: 20px;
@@ -100,7 +109,10 @@ header {
   text-decoration: underline;
 }
 
-
+.fas {
+  margin-right: 4px;
+}
+*/
 .content {
   padding: 20px;
 }
