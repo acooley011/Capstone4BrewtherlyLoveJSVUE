4d4b3251cf368dfe513ffab1dc731b9ca3092a07
Hannah Freeman <hannahjanefreeman@gmail.com>
2020-04-24 15:01:41 -0400

    Reviews Added to Schema" git push

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -3,17 +3,16 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS beers;
 DROP TABLE IF EXISTS breweries;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
-  username varchar(255) NOT NULL UNIQUE,     -- Username
+  username varchar(255) NOT NULL UNIQUE,  -- Username
   email varchar(255) NOT NULL,
   password varchar(32) NOT NULL,      -- Password
   salt varchar(256) NOT NULL,          -- Password Salt
   role varchar(255) NOT NULL default('beer-lover')
 );
-
-
 
 CREATE TABLE breweries (
     brewery_id serial,
@@ -29,8 +28,6 @@ CREATE TABLE breweries (
     CONSTRAINT pk_brewery_brewery_id PRIMARY KEY (brewery_id)
 );
 
-
-
 CREATE TABLE beers (
     beer_id serial,
     brewery_id integer,
@@ -41,6 +38,18 @@ CREATE TABLE beers (
     img_url varchar(255),
     CONSTRAINT pk_beer_beer_id PRIMARY KEY (beer_id),
     CONSTRAINT fk_brewery_brewery_id FOREIGN KEY (brewery_id) REFERENCES breweries(brewery_id)
+);
+
+CREATE TABLE reviews (
+	review_id serial,
+	beer_id integer NOT NULL,
+  name varchar(255) NOT NULL,
+  username varchar(255) NOT NULL UNIQUE,
+	rating varchar(30) NOT NULL,
+	average_rating varchar(30) NOT NULL
+  review varchar(299) NOT NULL,
+  CONSTRAINT pk_review primary key (review_id),
+  CONSTRAINT fk_beer_id foreign key (beer_id);
 );
 
 COMMIT TRANSACTION;
