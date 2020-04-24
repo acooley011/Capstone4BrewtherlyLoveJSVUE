3b46ec56ae96f5d2d019d86d5384c5027fb31fd0
Chet Sullivan <cksullivan13@gmail.com>
2020-04-23 20:49:11 -0400

    login page mostly complete

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username varchar(255) NOT NULL UNIQUE,     -- Username
+  email varchar(255) NOT NULL,
   password varchar(32) NOT NULL,      -- Password
   salt varchar(256) NOT NULL,          -- Password Salt
   role varchar(255) NOT NULL default('beer-lover')
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -10,7 +10,7 @@
 <main id="main-content" class="is-size-4 has-text-weight-semibold box has-text-centered text-box">
 
     <p> We keep track of all the breweries in Philadelphia so you don't have to! Browse our website and you will find a list of all the breweries and cider distilleries in Center City and surrounding neighborhoods. Make an account as a beer lover and you will be able to review any beers you try, along with interact with fellow beer lovers. Check out the review page for a list of all the reviewed beers sorted by average rating. Find out which breweries and beers are most popular at a glance.</p><br/>
-    <p>Own a brewery? Make a profile page if we don't already have one and keep track of your delicious brew stock for the public to see. Respond to customer questions on the message board too! Philadelphia has so many great places to go and now you wont have to worry about finding the best breweries in town.</p>
+    <p>Own a brewery? Make a profile page if we don't already have one and keep track of your delicious brew stock for the public to see. Respond to customer questions on the message board too! Philadelphia has so many great places to go and now we can drink up together in the City of Brew-therly Love.</p>
 </main>
 </section>
 </template>
