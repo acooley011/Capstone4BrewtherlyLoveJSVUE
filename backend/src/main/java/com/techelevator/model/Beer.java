07c9fbad83a672d2be501b0bcbe5262cd55910e4
shantal gonzalez <shantal.gonzalez@gmail.com>
2020-04-24 09:02:42 -0400

    editing beer model

---
+++ backend/src/main/java/com/techelevator/model/Beer.java
@@ -0,0 +1,99 @@
+package com.techelevator.model;
+
+import java.math.BigDecimal;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Beer {
+	
+	/*Properties*/
+	
+	private Long id;
+	
+	@NotBlank
+	private String name;
+	private String description;
+	private BigDecimal abv;
+	private String type;
+	private String imgUrl;
+	private boolean isActive = true;
+
+	
+	//TODO need to connect to Brewery and Reviews for Beerlist and review rating
+	//private int rating;
+	//private Long breweryId
+	//The Brewery model will need to use beerId
+	
+
+	/*GETTERS AND SETTERS*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getAbv() {
+		return abv;
+	}
+
+	public void setAbv(BigDecimal abv) {
+		this.abv = abv;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	/*method for start rating*/
+	//TODO need Review model rating data type TBD int/big decimal/float
+	
+	/*public int getStarRating() {
+		int starRating = 0;
+		
+		if (rating != null) {
+			starRating = rating.intValue();
+		}
+		return starRating;
+	}
+	
+	*/
+}
