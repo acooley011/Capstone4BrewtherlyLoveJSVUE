9dc63c8071e27c0125b82d402b1c5afd36f3f38f
Archie Cooley <acooley011@gmail.com>
2020-04-24 18:34:45 -0400

    trying to get a list of beers from a brewery

---
+++ frontend/src/views/BeerDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="beer-details">
+    <h1>{{ beer.name }}</h1>
+    <div class="beer">
+      <img v-bind:src="require('../assets/images/beers/' + beer.img)" />
+      <div class="details">
+        <div><span class="label">ABV:</span> {{ beer.abv }}</div>
+        <div><span class="label">Price:</span> ${{ beer.price }}</div>
+        <div><span class="label">Department:</span> {{ beer.brewery }}</div>
+        <div><span class="label">Description:</span> This is a description of this awesome product and you simply must try it out.</div>
+        <router-link to="/reviews" tag="button">Review this Beer</router-link>
+      </div>
+    </div>
+    <button v-on:click="goBack">GO BACK</button>
+  </div>
+</template>
+
+<script>
+//this is where the beers come from, its called products because I got it from old homework number 12
+import data from '../assets/data/products.json'
+
+export default {
+  name: 'beer',
+  data() {
+    return {
+      allBeers: data,
+      beer: null
+    }
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
+  },
+  created() {
+    this.product = this.allBeers.find(p => p.sku == this.$route.params.sku);
+  }
+}
+</script>
---
+++ frontend/src/views/Beers.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="products-list">
+    <h1>Beers from: {{ this.$route.params.brewery }}</h1>
+    <beer-list v-bind:beers="beers"/>
+  </div>
+</template>
+
+<script>
+//this is the beers by brewer
+import data from '../assets/data/beers.json'
+import BeerList from '@/components/BeerList.vue'
+
+export default {
+  name: 'products',
+  components: {
+    BeerList
+  },
+  data() {
+    return {
+      allBeers: data,
+      items: []
+    }
+  },
+  methods: {
+    getBeers(brewery) {
+      this.items = this.allBeers.filter((beer) => beer.brewery == brewery);
+    }
+  },
+  created() {
+   console.log("hello from product list in view");
+   const brewery = this.$route.params.department;
+   this.getProducts(brewery);
+  }
+}
+</script>
