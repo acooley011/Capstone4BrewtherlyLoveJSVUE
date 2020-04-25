bdf42d80461caa11e05ad9c7f4f2b17c1322539e
Chet Sullivan <cksullivan13@gmail.com>
2020-04-25 18:26:55 -0400

    ignore

--- frontend/src/views/BreweryList.vue
+++ frontend/src/views/BreweryList.vue
@@ -1,47 +1,47 @@
 <template>
+<body>
   <div id="brewery-list">
-    <h1>Brewery List</h1>
-    <ul>
-      <li>Yards</li>
-      <li>Evil Genius</li>
-      <li>LoveCity</li>
-      <li>DockStreet</li>
-      <li>ARS</li>
-      <li>Tired Hands</li>
-      <li>PBC</li>
-      <li>Original 13 Ciderworks</li>
-      <li>Seperatist</li>
-      <li>Crime and Punishment</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
-      <li>Filler</li>
+    <h1>Breweries</h1>
+    <p>filler text.</p>
+    <div class="brewery-list">
+      
+      <router-link v-for="brewery in breweries" v-bind:key="brewery.path" :class='brewery-list.class' v-bind:to="{path:'brewery-list' + brewery.path}" >
+        <img v-bind:src="require('../assets/images/Breweries' + brewery.banner)"/></router-link>
+    </div>
 
-    </ul>
   </div>
+  </body>
 </template>
 
 <script>
 export default {
-  name: 'brewery-list'
+  name: 'brewery-list',
+  data() {
+    return {
+      breweries: [
+        { path: '/brewery-list', banner: 'breweryARS.png' },
+        { path: '/brewery-list', banner: 'breweryCrimeAndPunishment.png'},
+        { path: '/brewery-list', banner: 'breweryDockStreet.png'},
+        { path: '/brewery-list', banner: 'breweryEvilGenius-MUSTEDIT.png' },
+        { path: '/brewery-list', banner: 'breweryLoveCity.png' },
+        { path: '/brewery-list', banner: 'breweryOriginal13.png' },
+        { path: '/brewery-list', banner: 'breweryPBC.png' },
+        { path: '/brewery-list', banner: 'brewerySeperatist.png' },
+        { path: '/brewery-list', banner: 'breweryTiredHands.png' },
+        { path: '/brewery-list', banner: 'breweryYards.png' }
+
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
 
-#brewery-list {
+.brewery-list {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
   text-align: center;
 }
 
