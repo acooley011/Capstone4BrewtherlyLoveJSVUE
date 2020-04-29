45f955eee3915278568c02f7b409d65cfdcb0b1d
Archie Cooley <acooley011@gmail.com>
2020-04-29 18:23:00 -0400

    reformat brewery

--- frontend/src/views/BreweryList.vue
+++ frontend/src/views/BreweryList.vue
@@ -1,22 +1,28 @@
 <template>
-<main class="main" >
-  <!-- add to main if we ever get this fetch to work v-if="breweryDetails !== null" -->
-<!-- this chunk of code was saved -->
-      <div class="is-size-4 has-text-weight-semibold box has-text-centered text-box"
-      v-for="brewery in breweryList.breweries" v-bind:key="brewery.id">
-    
-    
-    <h3><strong>{{brewery.name}}</strong></h3><br/>
-    <div class="columns">
-      <div>
-            <router-link :to="'/brewery/'+brewery.id"><img :src="brewery.breweryLogoUrl"></router-link>
-      </div>
-      <div id="brewery-info" class="text-box" >
-        <p>{{ brewery.description }}</p>
+  <main class="main">
+    <!-- add to main if we ever get this fetch to work v-if="breweryDetails !== null" -->
+    <!-- this chunk of code was saved -->
+    <div
+      class="is-size-4 has-text-weight-semibold box has-text-centered text-box"
+      v-for="brewery in breweryList.breweries"
+      v-bind:key="brewery.id"
+    >
+      <h3>
+        <strong>{{brewery.name}}</strong>
+      </h3>
+      <br />
+      <div class="columns">
+        <div>
+          <router-link :to="'/brewery/'+brewery.id">
+            <img :src="brewery.breweryLogoUrl" />
+          </router-link>
+        </div>
+        <div id="brewery-info" class="text-box">
+          <p>{{ brewery.description }}</p>
+        </div>
       </div>
     </div>
-    </div>
-    </main>
+  </main>
 </template>
 
 <!--<template>
@@ -33,59 +39,53 @@
 </template>-->
 
 <script>
-
 import auth from "../auth.js";
 
 export default {
-  name: 'brewery-list',
+  name: "brewery-list",
 
-  data(){
+  data() {
     return {
       breweryList: null,
-      breweryId: 1, //TODO during the create method, get the brewery id out of the url, see catalog HW
+      breweryId: 1 
     };
   },
 
   methods: {
-    fetchBrewery(){
+    fetchBrewery() {
       const options = {
         method: "GET",
-        headers:{
-          "Authorization" : `Bearer ${auth.getToken()}`
+        headers: {
+          Authorization: `Bearer ${auth.getToken()}`
           //If you are posting/putting want to add "Content-Type: application/json" -> setting the content type when sending content
-        },
+        }
         //If doing post/put then body: json.stringify would go here
       };
-    fetch(`${process.env.VUE_APP_REMOTE_API}/api/breweryList/`,options) 
-    .then(response => response.json())
-    .then(details => {
-      this.breweryList = details;
-      this.brewery = this.breweryList.brewery;
-      })
-    .catch(err => console.error(err));
+      fetch(`${process.env.VUE_APP_REMOTE_API}/api/breweryList/`, options)
+        .then(response => response.json())
+        .then(details => {
+          this.breweryList = details;
+          this.brewery = this.breweryList.brewery;
+        })
+        .catch(err => console.error(err));
     }
-
   },
 
-  created(){
-      //TODO Set brewery id here
-      this.fetchBrewery(); 
+  created() {
+    //TODO Set brewery id here
+    this.fetchBrewery();
   }
-
-  
-}
+};
 </script>
 
 
 <style scoped>
-
 #brewery-list {
   text-align: center;
- 
 }
 
 #brewery-info {
-font-size: 14px;
+  font-size: 14px;
 }
 
 p {
@@ -98,8 +98,5 @@ p {
 img {
   max-width: 50vw;
   min-width: 30vw;
-
 }
-
-
 </style>
